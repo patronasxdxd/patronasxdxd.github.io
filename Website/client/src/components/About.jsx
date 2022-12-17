60d310547b73eba18a19e21fa629092fc060c137
gilles zwijsen <gilles.zwijsen@student.kdg.be>
2022-12-17 17:32:19 +0100

    dwa

--- Website/client/src/components/About.jsx
+++ Website/client/src/components/About.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import styles from "/Users/gilleszwijsen/loan/client/src/css/mystyle.module.css"
+
 
 
 const About = () => {
@@ -6,17 +8,23 @@ const About = () => {
 
   return (
     
-    <div className="flex justify-center">
+    <div className="flex flex-col item-center justify-center">
 
+<div className= "m-auto">
 <img
     src="./images/me2.png  "
-    class="max-w-sm h-auto  transition-shadow ease-in-out duration-300 shadow-none hover:shadow-xl"
+    class="max-w-sm h-auto transition-shadow ease-in-out duration-300 shadow-none hover:shadow-xl"
     alt=""
   />
 
+  </div>
 
 
 
+
+<div className={styles.shadowxd2 }>
+     <h2 className="text-white text-4xl text-base text-center mx-2"> 0xPatronasxd  </h2>
+     </div>
 
 
 
