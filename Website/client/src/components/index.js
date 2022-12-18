0ec1f85a81f0ce9dec6b113bf267a8044ff017bb
gilles zwijsen <gilles.zwijsen@student.kdg.be>
2022-12-18 14:22:54 +0100

    11

--- Website/client/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Loader } from "./Loader";
-export { default as Navbar } from "./Navbar";
-export { default as Welcome } from "./Welcome";
-export { default as Skills} from "./Skills"
-export { default as Projects} from "./Projects"
-export { default as About} from "./About"
