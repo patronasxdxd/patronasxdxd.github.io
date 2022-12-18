bd44939ce1016d2ded672fdbb53c6d037fcbb133
gilles zwijsen <gilles.zwijsen@student.kdg.be>
2022-12-18 02:50:31 +0100

    temp

--- Dao/Dao/client/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Loader } from "./Loader";
-export { default as Navbar } from "./Navbar";
-export { default as Welcome } from "./Welcome";
-export { default as Box } from "./Box"
-export { default as Propose } from "./Propose"
-export { default as Vote } from "./Vote"
-export { default as Excecute } from "./Excecute"
