469f1ec7bb3d13a4c8e68dbc2aa06f3b8962f5f1
gilles zwijsen <gilles.zwijsen@student.kdg.be>
2022-12-26 23:38:16 +0100

    1111

--- Website/client/src/components/Projects.jsx
+++ Website/client/src/components/Projects.jsx
@@ -45,7 +45,7 @@ const Projects = () => {
 
 <div className="relative ">
     <a className="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-0 duration-300">
-    <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded animate-bounce" target="_blank" href="https://github.com/patronasxdxd/patronasxdxd.github.io">Code</a>
+    <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded animate-bounce" target="_blank" href="https://github.com/patronasxdxd/patronasxdxd.github.io/tree/main/Dao">Code</a>
     <a class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded animate-bounce" target="_blank" href="https://patronasxdxd.github.io/Daox">Demo</a>
     </a>
     <a href="https://patronasxdxd.github.io/Daox" className="relative">
@@ -55,6 +55,31 @@ const Projects = () => {
     </a>
   </div>
 
+
+  <div className="relative ">
+    <a className="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-0 duration-300">
+    <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded animate-bounce" target="_blank" href="https://github.com/patronasxdxd/Runescape">Code</a>
+    <a class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded animate-bounce" target="_blank" href="https://patronasxdxd.github.io/Runescape">Demo</a>
+    </a>
+    <a href="https://github.com/patronasxdxd/Runescape" className="relative">
+        <div className="flex flex-wrap content-center">
+            <img src="./images/Runescape.png" className="mx-auto  " alt=""></img>
+        </div>
+    </a>
+  </div>
+
+
+  <div className="relative">
+    <a className="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-0 duration-300">
+    <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded animate-bounce" target="_blank" href="https://github.com/patronasxdxd/Monopoly">Code</a>
+    <a class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded animate-bounce" target="_blank" href="https://patronasxdxd.github.io/Monopoly">Demo</a>
+    </a>
+    <a href="https://patronasxdxd.github.io/Daox" className="relative">
+        <div className="flex flex-wrap content-center">
+            <img src="./images/Monopoly.png" className="mx-auto "  alt=""></img>
+        </div>
+    </a>
+  </div>
 
 
 
--- Website/client/src/components/Welcome.jsx
+++ Website/client/src/components/Welcome.jsx
@@ -14,6 +14,45 @@ const Welcome = () => {
 
 
 <ol >
+
+<li class="border-l-2 border-green-600">
+    <div class="md:flex flex-start">
+      <div class="bg-green-600 w-6 h-6 flex items-center justify-center rounded-full -ml-3.5">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" class="text-white w-3 h-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path fill="currentColor" d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm64-192c0-8.8 7.2-16 16-16h288c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16v-64zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"></path>
+        </svg>
+      </div>
+      <div class="block p-6 rounded-lg shadow-lg bg-gray-100 max-w-md ml-6 mb-10">
+        <div class="flex justify-between mb-4">
+          <a href="#!" class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">Monopoly</a>
+          <a href="#!" class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">12 / 26 / 2022</a>
+        </div>
+        <p class="text-gray-700 mb-6">Monopoly Web3 game</p>
+        <a type="button" class="inline-block px-4 py-1.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out" href="https://github.com/patronasxdxd/Monopoly" data-mdb-ripple="true">Preview</a>
+        <a type="button" class="inline-block px-3.5 py-1 border-2 border-purple-600 text-purple-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" href="https://github.com/patronasxdxd/Monopoly" data-mdb-ripple="true">See demo</a>
+      </div>
+    </div>
+  </li>
+
+<li class="border-l-2 border-green-600">
+    <div class="md:flex flex-start">
+      <div class="bg-green-600 w-6 h-6 flex items-center justify-center rounded-full -ml-3.5">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" class="text-white w-3 h-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path fill="currentColor" d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm64-192c0-8.8 7.2-16 16-16h288c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16v-64zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"></path>
+        </svg>
+      </div>
+      <div class="block p-6 rounded-lg shadow-lg bg-gray-100 max-w-md ml-6 mb-10">
+        <div class="flex justify-between mb-4">
+          <a href="#!" class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">Runescape</a>
+          <a href="#!" class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">12 / 25/ 2022</a>
+        </div>
+        <p class="text-gray-700 mb-6">Runescape Web3 Game</p>
+        <a type="button" class="inline-block px-4 py-1.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" href="https://github.com/patronasxdxd/Runescape">Preview</a>
+        <a type="button" class="inline-block px-3.5 py-1 border-2 border-purple-600 text-purple-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" href="https://github.com/patronasxdxd/Runescape" data-mdb-ripple="true">See demo</a>
+      </div>
+    </div>
+  </li>
+
   <li class="border-l-2 border-purple-600">
     <div class="md:flex flex-start">
       <div class="bg-purple-600 w-6 h-6 flex items-center justify-center rounded-full -ml-3.5">
@@ -45,8 +84,8 @@ const Welcome = () => {
           <a href="#!" class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">12 / 01 / 2022</a>
         </div>
         <p class="text-gray-700 mb-6">Simple loan system </p>
-        <button type="button" class="inline-block px-4 py-1.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true">Preview</button>
-        <button type="button" class="inline-block px-3.5 py-1 border-2 border-purple-600 text-purple-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" data-mdb-ripple="true">See demo</button>
+        <a type="button" class="inline-block px-4 py-1.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out" href="https://github.com/patronasxdxd/Loan" data-mdb-ripple="true">Preview</a>
+        <a type="button" class="inline-block px-3.5 py-1 border-2 border-purple-600 text-purple-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" href="https://github.com/patronasxdxd/Loan" data-mdb-ripple="true">See demo</a>
       </div>
     </div>
   </li>
@@ -59,12 +98,12 @@ const Welcome = () => {
       </div>
       <div class="block p-6 rounded-lg shadow-lg bg-gray-100 max-w-md ml-6 mb-10">
         <div class="flex justify-between mb-4">
-          <a href="#!" class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">New Project</a>
+          <a href="#!" class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">Diamond Dao</a>
           <a href="#!" class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">21 / 12 / 2021</a>
         </div>
         <p class="text-gray-700 mb-6">Governor Dao, On a EIP-2535: Diamond, Multi-Facet Proxy</p>
-        <button type="button" class="inline-block px-4 py-1.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true">Preview</button>
-        <button type="button" class="inline-block px-3.5 py-1 border-2 border-purple-600 text-purple-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" data-mdb-ripple="true">See demo</button>
+        <a type="button" class="inline-block px-4 py-1.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out" href="https://github.com/patronasxdxd/patronasxdxd.github.io/tree/main/Dao" data-mdb-ripple="true">Preview</a>
+        <a type="button" class="inline-block px-3.5 py-1 border-2 border-purple-600 text-purple-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" href="https://github.com/patronasxdxd/patronasxdxd.github.io/tree/main/Dao" data-mdb-ripple="true">See demo</a>
       </div>
     </div>
   </li>
